bcdbce239bfa35636e7a585fe5527b03ce5a1191
Hǎiliàng Wáng <w@h12.me>
2015-08-06 17:42:32 +0800

    queue tests passed.

--- q.go
+++ q.go
@@ -3,26 +3,32 @@ package kafkaq
 import (
 	"encoding/json"
 	"errors"
-	"log"
-	"os"
+	"fmt"
 
 	"github.com/Shopify/sarama"
+	"github.com/stealthly/siesta"
 )
 
 func init() {
-	sarama.Logger = log.New(os.Stdout, "[Sarama] ", log.LstdFlags)
+	//sarama.Logger = log.New(os.Stdout, "[Sarama] ", log.LstdFlags)
 }
 
 type Config struct {
 	KafkaAddrs     []string
 	ZooKeeperAddrs []string
-	ConsumerGroup  string
 	Topic          string
+	ConsumerConfig
+}
+
+type ConsumerConfig struct {
+	ConsumerGroup string
+	Partition     int32
 }
 
 type Q struct {
-	p *Producer
-	c *Consumer
+	Name string
+	*Producer
+	*Consumer
 }
 
 func New(config *Config) (*Q, error) {
@@ -38,17 +44,20 @@ func New(config *Config) (*Q, error) {
 		return nil, err
 	}
 	return &Q{
-		p: p,
-		c: c,
+		Name:     config.Topic,
+		Producer: p,
+		Consumer: c,
 	}, nil
 }
 
-func (q *Q) Put(v interface{}) error {
-	return q.p.Send(v)
-}
-
-func (q *Q) Get(v interface{}) error {
-	return q.c.Get(v)
+func (q *Q) PopTo(o *Q, v interface{}) error {
+	if err := q.Peek(v); err != nil {
+		return err
+	}
+	if err := o.Put(v); err != nil {
+		return err
+	}
+	return q.Commit()
 }
 
 type Producer struct {
@@ -69,27 +78,86 @@ func NewProducer(config *Config) (*Producer, error) {
 	}, nil
 }
 
-func (p *Producer) Send(v interface{}) error {
+func (p *Producer) Put(v interface{}) error {
 	buf, err := json.Marshal(v)
 	if err != nil {
 		return err
 	}
+	return p.PutBytes(buf)
+}
+
+func (p *Producer) PutBytes(buf []byte) error {
 	message := &sarama.ProducerMessage{
 		Topic:     p.topic,
 		Partition: -1,
 		Value:     sarama.ByteEncoder(buf),
 	}
-	_, _, err = p.p.SendMessage(message)
+	_, _, err := p.p.SendMessage(message)
 	return err
 }
 
 type Consumer struct {
+	c     siesta.Connector
+	topic string
+	ConsumerConfig
+	offset int64
 }
 
 func NewConsumer(config *Config) (*Consumer, error) {
-	return &Consumer{}, nil
+	scfg := siesta.NewConnectorConfig()
+	scfg.BrokerList = config.KafkaAddrs
+	c, err := siesta.NewDefaultConnector(scfg)
+	if err != nil {
+		return nil, err
+	}
+	offset, err := c.GetOffset(config.ConsumerGroup, config.Topic, config.Partition)
+	if err != nil {
+		fmt.Println(err)
+		offset = 0
+	}
+	return &Consumer{
+		c:              c,
+		topic:          config.Topic,
+		ConsumerConfig: config.ConsumerConfig,
+		offset:         offset,
+	}, nil
 }
 
-func (c *Consumer) Get(v interface{}) error {
+func (c *Consumer) Pop(v interface{}) error {
+	if v != nil {
+		err := c.Peek(v)
+		if err != nil {
+			return err
+		}
+	}
+	return c.Commit()
+}
+
+func (c *Consumer) Commit() error {
+	c.offset++
+	c.c.CommitOffset(c.topic, c.topic, c.Partition, c.offset)
 	return nil
+}
+
+func (c *Consumer) Peek(v interface{}) error {
+	buf, err := c.PeekBytes()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(buf, v)
+}
+
+func (c *Consumer) PeekBytes() ([]byte, error) {
+	r, err := c.c.Fetch(c.topic, c.Partition, c.offset)
+	if err != nil {
+		return nil, err
+	}
+	messages := r.Data[c.topic][c.Partition].Messages
+	for _, msg := range messages {
+		if msg.Offset == c.offset {
+			return messages[0].Message.Value, nil
+		}
+	}
+	// TODO: handle buffer
+	return nil, errors.New("no data")
 }
--- q_test.go
+++ q_test.go
@@ -2,7 +2,10 @@ package kafkaq
 
 import (
 	"os"
+	"reflect"
 	"testing"
+
+	"h12.me/gspec/db/kafka"
 )
 
 var kafkaHost = func() string {
@@ -12,7 +15,6 @@ var kafkaHost = func() string {
 	}
 	return h
 }()
-
 var zooKeeperHost = func() string {
 	h := os.Getenv("ZOOKEEPER_HOST")
 	if h == "" {
@@ -20,46 +22,231 @@ var zooKeeperHost = func() string {
 	}
 	return h
 }()
+var zookeeper = kafka.ZooKeeper{Addr: zooKeeperHost}
 
 func TestPutGet(t *testing.T) {
+	topic, err := zookeeper.NewTopic(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer zookeeper.DeleteTopic(topic)
+
 	q, err := New(&Config{
 		KafkaAddrs:     []string{kafkaHost},
 		ZooKeeperAddrs: []string{zooKeeperHost},
-		Topic:          "kafkaq",
-		ConsumerGroup:  "kafkaq",
+		Topic:          topic,
+		ConsumerConfig: ConsumerConfig{
+			ConsumerGroup: topic,
+			Partition:     0,
+		},
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
 	var s string
 	{
-		c(t, q.Put("a"))
-		c(t, q.Get(&s))
+		if err := q.Put("a"); err != nil {
+			t.Fatal(err)
+		}
+		if err := q.Pop(&s); err != nil {
+			t.Fatal(err)
+		}
 		if s != "a" {
 			t.Fatal("expect a but got", s)
 		}
 	}
 	{
-		c(t, q.Put("a"))
-		c(t, q.Put("b"))
-		c(t, q.Put("c"))
-		c(t, q.Get(&s))
-		if s != "a" {
+		if err := q.Put("a"); err != nil {
+			t.Fatal(err)
+		}
+		if err := q.Put("b"); err != nil {
+			t.Fatal(err)
+		}
+		if err := q.Put("c"); err != nil {
+			t.Fatal(err)
+		}
+		if err := q.Pop(&s); err != nil || s != "a" {
 			t.Fatal("expect a but got", s)
 		}
-		c(t, q.Get(&s))
-		if s != "b" {
+		if err := q.Pop(&s); err != nil || s != "b" {
 			t.Fatal("expect b but got", s)
 		}
-		c(t, q.Get(&s))
-		if s != "c" {
+		if err := q.Pop(&s); err != nil || s != "c" {
 			t.Fatal("expect c but got", s)
 		}
 	}
 }
 
-func c(t *testing.T, err error) {
+func TestPutPop(t *testing.T) {
+	q := newIntQ(t, "kafka-put-pop", 1, 2, 3)
+	defer zookeeper.DeleteTopic(q.Name)
+	var i int
+	if err := q.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 1 {
+		t.Fatalf("expect 1 but got %d", i)
+	}
+	if err := q.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 2 {
+		t.Fatalf("expect 2 but got %d", i)
+	}
+	if err := q.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 3 {
+		t.Fatalf("expect 2 but got %d", i)
+	}
+}
+
+type testStruct struct {
+	I int
+	S string
+}
+
+func TestEncoding(t *testing.T) {
+	q := newQ(t, "kafka-encoding")
+	defer zookeeper.DeleteTopic(q.Name)
+	s := testStruct{
+		I: 1,
+		S: "a",
+	}
+	if err := q.Put(s); err != nil {
+		t.Fatal(err)
+	}
+	var got testStruct
+	if err := q.Pop(&got); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, s) {
+		t.Fatalf("expect %v, got %v", s, got)
+	}
+}
+
+func TestPeek(t *testing.T) {
+	name := "kafka-peek"
+	q := newIntQ(t, name, 1, 2)
+	defer zookeeper.DeleteTopic(q.Name)
+	for j := 0; j < 3; j++ {
+		var i int
+		if err := q.Peek(&i); err != nil {
+			t.Fatal(err)
+		}
+		if i != 1 {
+			t.Fatalf("expect 1 but got %d", i)
+		}
+	}
+	var i int
+	if err := q.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 1 {
+		t.Fatalf("expect 1 but got %d", i)
+	}
+	if err := q.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 2 {
+		t.Fatalf("expect 2 but got %d", i)
+	}
+}
+
+func TestResume(t *testing.T) {
+	name := "kafka-resume"
+	{
+		q := newIntQ(t, name, 1, 2)
+		defer zookeeper.DeleteTopic(q.Name)
+		var i int
+		if err := q.Pop(&i); err != nil {
+			t.Fatal(err)
+		}
+		if i != 1 {
+			t.Fatalf("expect 1 but got %d", i)
+		}
+	}
+	{
+		q := newQ(t, name)
+		var i int
+		if err := q.Pop(&i); err != nil {
+			t.Fatal(err)
+		}
+		if i != 2 {
+			t.Fatalf("expect 2 but got %d", i)
+		}
+	}
+}
+
+func TestPopDiscard(t *testing.T) {
+	q := newIntQ(t, "kafka-peek", 1, 2, 3)
+	defer zookeeper.DeleteTopic(q.Name)
+	if err := q.Pop(nil); err != nil {
+		t.Fatal(err)
+	}
+	var i int
+	if err := q.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 2 {
+		t.Fatalf("expect 2 but got %d", i)
+	}
+}
+
+func TestPopTo(t *testing.T) {
+	q1 := newIntQ(t, "kafka-popto-1", 1, 2)
+	defer zookeeper.DeleteTopic(q1.Name)
+	q2 := newIntQ(t, "kafka-popto-2", 3)
+	defer zookeeper.DeleteTopic(q2.Name)
+	var i int
+	if err := q1.PopTo(q2, &i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 1 {
+		t.Fatalf("expect 1 but got %d", i)
+	}
+	if err := q1.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 2 {
+		t.Fatalf("expect 2 but got %d", i)
+	}
+	if err := q2.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 3 {
+		t.Fatalf("expect 3 but got %d", i)
+	}
+	if err := q2.Pop(&i); err != nil {
+		t.Fatal(err)
+	}
+	if i != 1 {
+		t.Fatalf("expect 3 but got %d", i)
+	}
+}
+
+func newQ(t *testing.T, topic string) *Q {
+	q, err := New(&Config{
+		KafkaAddrs:     []string{kafkaHost},
+		ZooKeeperAddrs: []string{zooKeeperHost},
+		Topic:          topic,
+		ConsumerConfig: ConsumerConfig{
+			ConsumerGroup: topic,
+			Partition:     0,
+		},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
+	return q
+}
+
+func newIntQ(t *testing.T, name string, is ...int) *Q {
+	q := newQ(t, name)
+	for _, i := range is {
+		if err := q.Put(i); err != nil {
+			t.Fatal(err)
+		}
+	}
+	return q
 }
